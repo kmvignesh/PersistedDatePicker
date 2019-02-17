204a6bcc77097929f8b1ac626f5604268aa1c855
kmvignesh <kmvignesh94@gmail.com>
2019-02-17 16:50:08 +0530

    Initial commit

---
+++ app/src/main/java/in/codeandroid/persisteddatepicker/MainActivity.kt
@@ -0,0 +1,51 @@
+package `in`.codeandroid.persisteddatepicker
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.US)
+    private val timeFormat = SimpleDateFormat("hh:mm a", Locale.US)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val now = Calendar.getInstance()
+        btn_date.text = dateFormat.format(now.time)
+        btn_time.text = timeFormat.format(now.time)
+
+
+        btn_date.setOnClickListener {
+            val datePicker = DatePickerDialog(
+                this, DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    now.set(Calendar.YEAR, year)
+                    now.set(Calendar.MONTH, month)
+                    now.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    btn_date.text = dateFormat.format(now.time)
+                },
+                now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH)
+            )
+            datePicker.show()
+        }
+
+        btn_time.setOnClickListener {
+            val timePicker = TimePickerDialog(
+                this, TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                    now.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    now.set(Calendar.MINUTE, minute)
+                    btn_time.text = timeFormat.format(now.time)
+                },
+                now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), false
+            )
+            timePicker.show()
+        }
+
+    }
+}
